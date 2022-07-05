262e8691b358a417affdf51ed39a44fed982e4ea
unknown <sabu0550@gmail.com>
2022-07-05 14:11:51 +0200

    add income funcation

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 let amount;
 let transactionName;
 
-let income = 0
+// varables
+let income = 0;
+let expense = 0;
+let total = 0;
+
+// add income funcation
+function addIncome() {
+    getUserInput();
+
+    if (isValid(amount) && isValid(transactionName)) {
+        tableData.push({
+            id: tableData.length + 1,
+            transaction: transactionName,
+            type: 'income',
+            amount,
+        });
+    }
+}
