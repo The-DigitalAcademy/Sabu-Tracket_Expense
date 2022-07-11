da6cdfd651e185be85e309dd5e1ee2a84cfe6c1d
unknown <sabu0550@gmail.com>
2022-07-11 09:28:36 +0200

    last display

--- script.js
+++ script.js
@@ -134,3 +134,16 @@ function removeTransaction(index) {
     }
 }
 
+//clear table
+function clearTableData() {
+    if (tableData.length == 0) {
+        alert('Transaction table is aleady empty');
+        return;
+    }
+    if (window.confirm('Are you sure to clear   Transaction table')) {
+        tableData = [];
+        calculate();
+        displayTableData();
+    }
+}
+
